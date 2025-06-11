96c7dec71013b394554c52e1891bad12bdcadeb5
Grace0127 <148304328+Grace0127@users.noreply.github.com>
2025-06-11 21:49:01 +0800

    Add files via upload

--- code/funtion_app.py
+++ code/funtion_app.py
@@ -1,11 +1,16 @@
 import logging
 import azure.functions as func
 
+app = func.FunctionApp(http_auth_level=func.AuthLevel.ANONYMOUS)
+
+@app.route(route="clickCounter")
+
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('📥 收到一個點擊請求')
 
     return func.HttpResponse(
-        '成功',
+        '---測試成功!---',
         status_code=200,
         mimetype="application/json"
     )
+    
--- code/main.js
+++ code/main.js
@@ -1,9 +1,10 @@
 console.log("✅ main.js 載入成功");
 
 let score = 0;
-let currentColor = "紅";
+let currentColor = "彩蛋_紅";
 const egg = document.getElementById("egg");
-const colors = ["紅", "綠", "藍", "萬聖節_1", "萬聖節_2", "聖誕節_1", "聖誕節_2", "聖誕節_3", "新年_1", "新年_2"];
+const colors = ["彩蛋_紅", "彩蛋_綠", "彩蛋_藍", "萬聖節_1", "萬聖節_2", "聖誕節_1", "聖誕節_2", "聖誕節_3", "新年_1", "新年_2", "新年_3", "情人節", "感恩節", "中秋節_1", "中秋節_2", "水豚"];
+console.log("目前分數：" + score);    
 
 function getRandomColor() {
   const index = Math.floor(Math.random() * colors.length);
@@ -11,12 +12,12 @@ function getRandomColor() {
 }
 
 function popEgg() {
-  egg.src = `彩蛋_${currentColor}_破.png`;
+  egg.src = `${currentColor}_破.png`;
   score += 1;
   document.getElementById("score").innerText = "分數：" + score;
 
-  // 呼叫 Azure Function（記得換成你的 URL）https://你的-function-url.azurewebsites.net/api/clickCounter?
-  fetch("https://poppopping.azurewebsites.net/api/http_trigger1?", {
+  // 呼叫 Azure Function（記得換成你的 URL）例:https://你的-function-url.azurewebsites.net/api/clickCounter?
+  fetch("#請填入你的 Azure Function URL", {
     method: "POST"
   }).then(res => {
     console.log("✅ Azure Function 回應", res.status);
@@ -28,7 +29,7 @@ function popEgg() {
 
 function resetEgg() {
   currentColor = getRandomColor();
-  egg.src = `彩蛋_${currentColor}.png`;
+  egg.src = `${currentColor}.png`;
 }
 
 egg.addEventListener("mousedown", popEgg);
